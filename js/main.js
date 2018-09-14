41bf308aad9dbb63287b0daea315553bb754abe5
Den <den4ikk007@gmail.com>
2018-09-15 02:32:27 +0400

    running pixel

--- js/main.js
+++ js/main.js
@@ -2,18 +2,18 @@
 var game = {
     isRunning: false,
     score: 0,
-    sizeX: 20,
-    sizeY: 20,
-    snake: {
-        direction: "up",
-        len: 2,
-        speed: 200,
-        headCoordX: 0,
-        headCoordY: 0,
-    }
+    sizeX: 40,
+    sizeY: 40,
 }
-game.snake.headCoordX = Math.round(game.sizeX / 2);
-game.snake.headCoordY = Math.round(game.sizeY / 2);
+var snake = {
+    body: [],
+    direction: "up",
+    colour: "#ffff00",
+    speed: 200,
+    headCoordX: Math.round(game.sizeX / 2),
+    headCoordY: Math.round(game.sizeY / 2),
+}
+snake.body.push([snake.headCoordX, snake.headCoordY]); //координаты "головы"
 
 
 
@@ -26,38 +26,76 @@ for (var i = 0; i < game.sizeY; i++) {
     for (var j = 0; j < game.sizeX; j++) {
         var cell = document.createElement("div");
         cell.classList.add("cell");
-        cell.setAttribute("x", j); //даем ячейкам атрибуты с координатами
-        cell.setAttribute("y", i);
+        cell.classList.add("x" + j + "y" + i);
         fieldRow.appendChild(cell);
 
     }
     field.appendChild(fieldRow);
 }
 
+document.querySelector(".x" + snake.body[0][0] + "y" + snake.body[0][0]).classList.add("snake"); //рисуем голову
 
-addEventListener("keydown",function(e){//смена направления движения по клавишам
-//    console.log(e.key);
-    switch(e.key){
+
+addEventListener("keydown", function (e) { //смена направления движения по клавишам
+    //    console.log(e.key);
+    switch (e.key) {
         case "ArrowUp":
-            if(game.snake.direction!="down")
-            game.snake.direction="up";
+            if (snake.direction != "down")
+                snake.direction = "up";
             break;
         case "ArrowLeft":
-            if(game.snake.direction!="right")
-            game.snake.direction="left";
+            if (snake.direction != "right")
+                snake.direction = "left";
             break;
         case "ArrowRight":
-            if(game.snake.direction!="left")
-            game.snake.direction="right";
+            if (snake.direction != "left")
+                snake.direction = "right";
             break;
         case "ArrowDown":
-            if(game.snake.direction!="up")
-            game.snake.direction="down";
+            if (snake.direction != "up")
+                snake.direction = "down";
+            break;
+        case "Enter":
+            game.isRunning = true;
+            console.log("game is running...");
             break;
     }
-//    console.log(game.snake.direction);
+    //    console.log(snake.direction);
 })
 
-function move(){
-    
+function move() {
+    var headX = snake.body[0][0];
+    var headY = snake.body[0][1];
+    var tailX = snake.body[snake.body.length - 1][0];
+    var tailY = snake.body[snake.body.length - 1][1];
+
+    switch (snake.direction) {
+        case "up":
+            document.querySelector(".x" + headX + "y" + headY).classList.remove("snake");
+            document.querySelector(".x" + headX + "y" + (headY - 1)).classList.add("snake");
+            snake.body[0] = [headX, headY - 1];
+            break;
+        case "down":
+            document.querySelector(".x" + headX + "y" + headY).classList.remove("snake");
+            document.querySelector(".x" + headX + "y" + (headY + 1)).classList.add("snake");
+            snake.body[0] = [headX, headY + 1];
+            break;
+        case "left":
+            document.querySelector(".x" + headX + "y" + headY).classList.remove("snake");
+            document.querySelector(".x" + (headX - 1) + "y" + headY).classList.add("snake");
+            snake.body[0] = [headX - 1, headY];
+            break;
+        case "right":
+            document.querySelector(".x" + headX + "y" + headY).classList.remove("snake");
+            document.querySelector(".x" + (headX + 1) + "y" + headY).classList.add("snake");
+            snake.body[0] = [headX + 1, headY];
+            break;
+    }
+    console.log("move");
 }
+
+
+
+
+    setInterval(move, snake.speed);
+
